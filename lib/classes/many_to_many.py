22f4db3f1f2c2dddd374840a234767067b8f2f00
Clarence Kinuthia <mclarence7311@gmail.com>
2024-05-28 09:43:28 +0300

    passing with no comments

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,3 +1,47 @@
+class Article:
+    all = []  # Initialize an empty list to hold all articles
+
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author")
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be an instance of Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise Exception("Title must be a string between 5 and 50 characters")
+
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        author.articles().append(self)
+        magazine.articles().append(self)
+
+        # Add the created article to the 'all' attribute
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise Exception("Magazine must be an instance of Magazine")
+        self._magazine = value
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception("Author must be an instance of Author")
+        self._author = value
+        
 class Author:
     def __init__(self, name):
         if not isinstance(name, str) or len(name) == 0:
@@ -72,8 +116,16 @@ class Magazine:
         return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        authors = set(article.author for article in self._articles if self._articles.count(article) > 2)
-        return list(authors) if authors else None
+        if not self._articles:
+            return None
+        author_count = {}
+        for article in self._articles:
+            if article.author in author_count:
+                author_count[article.author] += 1
+            else:
+                author_count[article.author] = 1
+        return [author for author, count in author_count.items() if count > 2]
+
 
     @classmethod
     def top_publisher(cls):
@@ -82,46 +134,3 @@ class Magazine:
         return max(cls._all_magazines, key=lambda magazine: len(magazine.articles()))
 
 
-class Article:
-    all = []  # Initialize an empty list to hold all articles
-
-    def __init__(self, author, magazine, title):
-        if not isinstance(author, Author):
-            raise Exception("Author must be an instance of Author")
-        if not isinstance(magazine, Magazine):
-            raise Exception("Magazine must be an instance of Magazine")
-        if not isinstance(title, str) or not (5 <= len(title) <= 50):
-            raise Exception("Title must be a string between 5 and 50 characters")
-
-        self._author = author
-        self._magazine = magazine
-        self._title = title
-        author.articles().append(self)
-        magazine.articles().append(self)
-
-        # Add the created article to the 'all' attribute
-        Article.all.append(self)
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def author(self):
-        return self._author
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, value):
-        if not isinstance(value, Magazine):
-            raise Exception("Magazine must be an instance of Magazine")
-        self._magazine = value
-
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise Exception("Author must be an instance of Author")
-        self._author = value
